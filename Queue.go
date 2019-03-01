1a5cfe0f7cd5dfbdce0fbaacb6de2fee7d31a995
S1lva <emillatypov9335@gmail.com>
2019-03-01 11:58:14 +0300

    Added Size and IsEmpty functions, added simple tutorial

--- Queue.go
+++ Queue.go
@@ -1,4 +1,6 @@
-package Queue
+package main
+
+import "fmt"
 
 type Queue []int
 
@@ -12,6 +14,34 @@ func (q *Queue) Poll() int {
 	return value
 }
 
-func (q *Queue) Add(val int){
-	*q = append(*q, val)
+func (q *Queue) Add(val ... int) {
+	*q = append(*q, val...)
+}
+
+func (q *Queue) Size() int {
+	return len(*q)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.Size() == 0
+}
+
+func main() {
+	queue := make(Queue, 0) // create new Queue
+	fmt.Println("This is empty Queue ", queue)
+	fmt.Println("		IsEmpty: ", queue.IsEmpty())
+	queue.Add(18, 99, 44, 13, 22, -67)
+
+	fmt.Println("\nThis is filled Queue: ", queue)
+	fmt.Println("\nLet's check first element without removing:")
+	fmt.Println("		first element: ", queue.Peek())
+	fmt.Println("		Queue: ", queue)
+	fmt.Println("		Queue size: ", queue.Size())
+	fmt.Println("		IsEmpty: ", queue.IsEmpty())
+	fmt.Println("\nLet's remove  and second elements:")
+	fmt.Println("		first element: ", queue.Poll())
+	fmt.Println("		second element: ", queue.Poll())
+	fmt.Println("		Queue: ", queue)
+	fmt.Println("		Queue size: ", queue.Size())
+	fmt.Println("		IsEmpty: ", queue.IsEmpty())
 }
